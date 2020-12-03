def38c0864714b4e300b459c0daa5c328d32c5b0
FanGaoXS <954278478@qq.com>
2020-12-03 16:15:16 +0800

    market@wqk 创建仓库

---
+++ market-server/src/test/java/cn/wqk/marketserver/MarketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.wqk.marketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
